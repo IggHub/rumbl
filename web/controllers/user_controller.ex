d2fb21f25e62e29e6e9cb3fbc54a8a2e2ef12e77
Iggy <igoririanto@rocketmail.com>
2018-05-15 07:52:37 -0500

    change config to use ecto again, edit all, get, and get_by to all_user, get_user, and get_user_by

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,12 +3,12 @@ defmodule Rumbl.UserController do
   alias Rumbl.User
 
   def index(conn, _params) do
-    users = Repo.all User
+    users = Repo.all_user User
     render conn, "index.html", users: users
   end
 
   def show(conn, %{"id"=> id}) do
-    user = Repo.get User, id
+    user = Repo.get_user User, id
     render conn, "show.html", user: user
   end
 end
