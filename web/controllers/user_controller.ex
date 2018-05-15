a560fc1aa7f6828a00a10f9c6769fa93c69a508b
apolloguest <nithya.sam@mrcooper.com>
2018-05-15 17:56:35 -0500

    add new user model, templates and routing

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,12 +3,17 @@ defmodule Rumbl.UserController do
   alias Rumbl.User
 
   def index(conn, _params) do
-    users = Repo.all_user User
+    users = Repo.all User
     render conn, "index.html", users: users
   end
 
   def show(conn, %{"id"=> id}) do
-    user = Repo.get_user User, id
+    user = Repo.get User, id
     render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 end
