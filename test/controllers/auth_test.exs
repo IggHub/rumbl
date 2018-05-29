1ab35021abd96f4e8b6121c38e1eae127ed6ace1
Iggy <igoririanto@rocketmail.com>
2018-05-29 18:09:59 -0500

    add testing for user logins

--- test/controllers/auth_test.exs
+++ test/controllers/auth_test.exs
@@ -31,4 +31,42 @@ defmodule Rumbl.AuthTest do
     next_conn = get(login_conn, "/")
     assert get_session(next_conn, :user_id) == 123 
   end
+
+  test "logout drops user session", %{conn: conn} do
+    logout_conn = conn
+                    |> put_session(:user_id, 123)
+                    |> Auth.logout()
+                    |> send_resp(:ok, "")
+    next_conn = get(logout_conn, "/")
+    refute get_session(next_conn, :user_id)
+  end
+
+  test "call gets user_id", %{conn: conn} do
+    user = insert_user()
+    conn = conn
+            |> put_session(:user_id, user.id)
+            |> Auth.call(Repo)
+    assert conn.assigns.current_user.id == user.id
+  end
+
+  test "call with no sessions sets current_user assign to nil", %{conn: conn} do
+    conn = Auth.call(conn, Repo)
+    assert conn.assigns.current_user == nil
+  end
+
+  test "login with valid username and pass", %{conn: conn} do
+    user = insert_user(username: "iggy", password: "secret")
+    {:ok, conn} = Auth.login_by_username_and_pass(conn, "iggy", "secret", repo: Repo)
+    
+    assert conn.assigns.current_user.id == user.id 
+  end
+
+  test "login with a not found user", %{conn: conn} do
+    assert {:error, :not_found, _conn} = Auth.login_by_username_and_pass(conn, "iggy", "secret", repo: Repo)
+  end
+
+  test "login with a mismatched password", %{conn: conn} do
+    user = insert_user(username: "iggy", password: "secret")
+    assert {:error, :unauthorized, _conn} = Auth.login_by_username_and_pass(conn, "iggy", "wrong_password", repo: Repo)
+  end
 end
