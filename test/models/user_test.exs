c3275c7b4cb35aecf02df3a722329da2a0af86b7
Iggy <igoririanto@rocketmail.com>
2018-05-30 18:11:39 -0500

    add user and category repo tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -17,6 +17,25 @@ defmodule Rumbl.UserTest do
 
   test "changeset that does not accept username that is too long" do
     attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 100))
-    assert {:username, {"should be at most %{count} character(s)", [count: 20]}} in errors_on(%Users{}, attrs)
+    assert {:username, "should be at most 20 character(s)"} in errors_on(%User{}, attrs)
+  end
+
+  test "registration_changeset password must be at least 6 chars long" do
+    attrs = Map.put(@valid_attrs, :password, "12345")
+    changeset = User.registration_changeset(%User{}, attrs)
+
+    assert {:password, {"should be at least %{count} character(s)",
+          [count: 6, validation: :length, min: 6]}} in changeset.errors()
+  end
+
+  test "registration_changeset with valid password hashes it" do
+    attrs = Map.put(@valid_attrs, :password, "123456")
+    changeset = User.registration_changeset(%User{}, attrs)
+
+    %{password: pass, password_hash: pass_hash} = changeset.changes
+
+    assert changeset.valid?
+    assert pass_hash
+    assert Comeonin.Bcrypt.checkpw(pass, pass_hash)
   end
 end
