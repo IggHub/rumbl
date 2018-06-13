d6552aa94296a29acfba18511901c0c5609ced8e
Iggy <igoririanto@rocketmail.com>
2018-06-13 18:18:44 -0500

    add hypothetical wolfram backend for infoSys

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -1,25 +1,19 @@
 defmodule Rumbl do
   use Application
 
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec
 
-    # Define workers and child supervisors to be supervised
     children = [
       supervisor(Rumbl.Repo, []),
+      supervisor(Rumbl.InfoSys.Supervisor, []),
       supervisor(Rumbl.Endpoint, []),
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Rumbl.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     Rumbl.Endpoint.config_change(changed, removed)
     :ok
